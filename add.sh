da6fd8f0d61c1e8448ca11888156ce47d4f1a089
leafcxy <cxy1337@gmail.com>
2023-09-05 17:18:15 +0800

    ++

--- add.sh
+++ add.sh
@@ -8,8 +8,8 @@ exit
 fi
 
 # 考虑时区的问题
-_date=`date '+%Y-%m-%d' -d "-1days"`
-_time=`date '+%Y-%m-%d %H:%M:%S' -d "-1days"`
+_date=`date '+%Y-%m-%d'`
+_time=`date '+%Y-%m-%d %H:%M:%S'`
 _file="./_posts/${_date}-$1.md"
 echo $_time
 
@@ -17,8 +17,8 @@ printf "%s%s%s\r\n" '-' '-' '-' > $_file
 
 printf "title: $1\r\n" >> $_file
 printf "date: ${_time}\r\n" >> $_file
-printf "categories:\r\n" >> $_file
-printf "tags:\r\n" >> $_file
+printf "categories: [default]\r\n" >> $_file
+printf "tags: [Jekyll, default]\r\n" >> $_file
 
 printf "%s%s%s\r\n" '-' '-' '-' >> $_file
 
